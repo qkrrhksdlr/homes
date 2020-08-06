572d124d2af13c9219b377505204007d04704030
user <user@LAPTOP-BMDLV7EQ>
2020-08-06 18:25:23 +0900

    kkk

--- spring_prj/src/main/java/com/kpc/board/BoardController.java
+++ spring_prj/src/main/java/com/kpc/board/BoardController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.kpc.board.BoardVO;
-//dkdkdkdkdk
+//dkdkdkdkdkkk
 @Controller
 public class BoardController {
 	
